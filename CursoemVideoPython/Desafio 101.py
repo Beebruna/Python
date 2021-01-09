bdbe10ea76acca1b417f5960db0aae8be44e0af3
Débora Bruna <beebruna@outlook.com>
2021-01-08 22:29:33 -0300

    desafios 101 e 102 feitos

--- CursoemVideoPython/Desafio 101.py
+++ CursoemVideoPython/Desafio 101.py
@@ -1,3 +1,24 @@
 '''
 Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.
 '''
+
+from datetime import date
+
+def voto(anoNascimento):
+
+    idade = date.today().year - anoNascimento
+
+    if idade < 16:
+        return idade, 'NEGADO'
+    else:
+        if 16 <= idade < 18 or idade >= 70:
+            return idade, 'OPCIONAL'
+        else:
+            return idade, 'OBRIGATÓRIO'
+
+
+print('-'*30)
+anoNascimento = int(input('Em que ano você nasceu? '))
+idade, voto = voto(anoNascimento)
+print(f'Com {idade} anos: VOTO {voto}.')
+
--- CursoemVideoPython/Desafio 102.py
+++ CursoemVideoPython/Desafio 102.py
@@ -1,3 +1,40 @@
 '''
 Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e o outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
 '''
+
+def fatorial(valor, show=False):
+
+    """
+    -> Calcula o Fatorial de um número.
+    :param valor: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor do Fatorial do número passado.
+    """
+
+    fatorial = 1
+
+    for i in range(valor, 0, -1):
+        fatorial *= i
+
+        if show:
+            print(i, end='')
+
+            if i == 1:
+                print(end=' = ')
+            else:
+                print(end=' x ')
+
+    return fatorial
+
+
+valor = int(input('Digite um valor: '))
+show = str(input('Deseja ver os cálculos? [S/N] '))
+
+if show in 'Ss':
+    print(f'O fatorial de {valor} é:')
+    show = True
+    print(f'{fatorial(valor, show)}')
+else:
+    print(f'O fatorial de {valor} é: {fatorial(valor)}')
+
+input()
